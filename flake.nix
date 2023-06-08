be54ce61cd3034bc977b392249d98641fc3e72c1
Heneli Kailahi <heneli@kailahi.com>
2023-06-08 14:04:49 -0700

    Upgrade to nixpkgs/(22.11xUnstable mix) to nixpkgs/release-23.05. Upgrade nix 2.13 -> 2.15

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/release-23.05";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
